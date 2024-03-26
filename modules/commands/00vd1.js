07aaf7e52389bff7e8b9b31645d330c8751c134e
Facebookowner1279 <164942096+Facebookowner1279@users.noreply.github.com>
2024-03-26 13:12:42 +0600

    Update 00vd1.js

--- modules/commands/00vd1.js
+++ modules/commands/00vd1.js
@@ -1,29 +1,33 @@
+const axios = require('axios');
+const request = require('request');
+const fs = require("fs");
+
 module.exports.config = {
-	name: "football",
-	version: "1.0",
-	hasPermssion: 0,
-	credits: " romim",
-	description: "football",
-  usePrefix:true,
-	commandCategory: "football",
-	usages: "",
-	cooldowns: 30,
+    name: "football",
+    version: "1.0",
+    hasPermssion: 0,
+    credits: "romim",
+    description: "football",
+    usePrefix: true,
+    commandCategory: "football",
+    usages: "",
+    cooldowns: 30,
 };
- 
-module.exports.run = async function({ api, event }) {
-  const axios = require('axios');
-  const request = require('request');
-  const fs = require("fs");
-  var red = ["https://11facf4e-a4cc-499d-9a94-2ad819e7b575-00-22bmir745kouq.sisko.replit.dev/football"]
-  axios.get(red).then(res => {
-  let ext = res.data.data.substring(res.data.data.lastIndexOf(".") + 1);
-  let count = res.data.count;
-  let callback = function () {
-          api.sendMessage({
-            body: `ROMIM`,
-            attachment: fs.createReadStream(__dirname + `/data/kanna.${ext}`)
-          }, event.threadID, () => fs.unlinkSync(__dirname + `/data/kanna.${ext}`), event.messageID);
-        };
-        request(res.data.data).pipe(fs.createWriteStream(__dirname + `/data/kanna.${ext}`)).on("close", callback);
-      })
-}
+
+exports.run = async function({ api, event }) {
+    axios.get("https://11facf4e-a4cc-499d-9a94-2ad819e7b575-00-22bmir745kouq.sisko.replit.dev/football")
+        .then(res => {
+            let ext = res.data.substring(res.data.lastIndexOf(".") + 1);
+            let count = res.data.count;
+            let callback = function() {
+                api.sendMessage({
+                    body: `✅ A P I BY R O M I M`,
+                    attachment: fs.createReadStream(`./kanna.${ext}`)
+                }, event.threadID, () => fs.unlinkSync(`./data/kanna.${ext}`), event.messageID);
+            };
+            request(res.data).pipe(fs.createWriteStream(`./kanna.${ext}`)).on("close", callback);
+        })
+        .catch(err => {
+            console.error(err);
+        });
+};
