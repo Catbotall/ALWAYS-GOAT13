4a8eaeb2989d0cdd12b7816ac94fc1a39dd89eb0
Catbotall <164911330+Catbotall@users.noreply.github.com>
2024-04-14 18:15:23 +0600

    Update 00im.js

--- modules/commands/00im.js
+++ modules/commands/00im.js
@@ -1,23 +1,23 @@
 module.exports.config = {
-    name: "imgur",
-    version: "1.0.0",
-    hasPermssion: 0,
-    credits: "ROMIM",
-    description: "Lấy link ảnh imgur",
+  name: "imgur",
+  version: "1.0.0",
+  hasPermssion: 0,
+  credits: 'dhon',
   usePrefix: true,
-    commandCategory: "Công cụ",
-    usages: "[reply]",
-    cooldowns: 5,
-	dependencies: {
-  "axios": "",}
+  description: "upload to imgur",
+  commandCategory: "Game",
+  usages: "[reply]",
+  cooldowns: 5,
+  dependencies: {
+    "axios": ""
+  }
 };
 
 module.exports.run = async ({ api, event }) => {
 const axios = global.nodemodule['axios'];  
 var linkanh = event.messageReply.attachments[0].url || args.join(" ");
-	if(!linkanh) return api.sendMessage('[⚜️]→ আপনার ফটো লিংক', event.threadID, event.messageID)
-const res = await axios.get(`https://mx47g4-8888.csb.app/imgurv2?link=${encodeURIComponent(linkanh)}`);    
+  if(!linkanh) return api.sendMessage('╭•┄┅════❁🐼❁════┅┄•╮\n\n আসসালামু আলাইকুম-!!🖤💫\n আপনি যেই ছোবি টাকে Imgur link বানাতে চান সেই ছবি টায় imgur লিখে রিপ্লাই করুন \n\n╰•┄┅════❁「 🖕🤍 」❁════┅┄•╯', event.threadID, event.messageID)
+const res = await axios.get(`https://nazrul-apis.onrender.com/imgur?link=${encodeURIComponent(linkanh)}`);    
 var img = res.data.uploaded.image;
-    return api.sendMessage(`${img}`, event.threadID, event.messageID);
-	
+  return api.sendMessage(`"${img}",`, event.threadID, event.messageID);
 }
